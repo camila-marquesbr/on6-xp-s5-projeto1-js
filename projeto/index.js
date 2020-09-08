4ebb76362d918aa1c9d348a1928c5d32fa621715
camila-marquesbr <camiladev.br@gmail.com>
2020-09-08 10:16:20 -0300

    Adicionando Tabela

--- projeto/index.js
+++ projeto/index.js
@@ -5,19 +5,11 @@ console.log('--------------------------------------')
 const db = require('./database')
 
 //Exercício 1
-const listarTabela= []
 
-function adicionaProduto(db){
-    return listarTabela.push(db);
-}
+console.table(db)
 
-console.log(db, 'Seu carrinho está vazio!')
-adicionaProduto(db[0]);
-adicionaProduto(db[1]);
-adicionaProduto(db[2]);
-adicionaProduto(db[3]);
-adicionaProduto(db[4]);
-adicionaProduto(db[5]);
-adicionaProduto(db[6]);
-adicionaProduto(db[7]);
-console.log(listarTabela, 'Produtos adicionados');
+//Exercício 2
+
+const readline = require('readline-sync')
+const inputId = parseFloat(readline.question("Qual o ID do produto?"))
+const inputQuantidade = parseFloat(readline.question("Qual a quantidade? "))
