0d269e8abe6a96a05e4aa93da8809a204e9b8ffd
camila-marquesbr <camiladev.br@gmail.com>
2020-09-08 19:57:22 -0300

    ordenarPreco

--- projeto/index.js
+++ projeto/index.js
@@ -8,8 +8,26 @@ const {products} = db
 //Exercício 1
 
 console.table(products)
+
+function ordenarPreco(a,b){
+    return a.preco - b.preco
+}
+products.sort(ordenarPreco)
+
+console.table(products)
+
 //Exercício 2
 
 const readline = require('readline-sync')
 const inputId = parseFloat(readline.question("Qual o ID do produto?"))
-const inputQuantidade = parseFloat(readline.question("Qual a quantidade? "))
+const inputQuantities = parseFloat(readline.question("Qual a quantidade  que deseja comprar? "))
+const inputDiscount = parseFloat(readline.question("Você tem algum cupom de desconto? Se sim digite aqui:"))
+
+//function discount{
+    //if (inputDiscount == true) {
+    //    let totalPriceDiscont = totalPrice * 0,1
+    //   let amountPaid = totalPrice - totalPriceDiscount
+   // }else{
+    //    console.log("Sem descontos hoje!")
+  //  }
+//}
